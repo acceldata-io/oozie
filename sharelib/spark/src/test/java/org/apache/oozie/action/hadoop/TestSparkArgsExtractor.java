275d27fb2bffa8c2db3c4f9b4336193bdc35c179
jacktheone <makai.janos1996@gmail.com>
2026-04-23 21:28:01 +0530

    OOZIE-3586

--- sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkArgsExtractor.java
+++ sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkArgsExtractor.java
@@ -20,6 +20,7 @@ package org.apache.oozie.action.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.oozie.action.ActionExecutorException;
 import org.junit.After;
 import org.junit.Test;
 
@@ -514,6 +515,51 @@ public class TestSparkArgsExtractor {
         checkAndDeleteFile(SPARK_DEFAULTS_GENERATED_PROPERTIES);
         checkAndDeleteFile(SPARK_DEFAULTS_PROPERTIES);
     }
+
+    @Test
+    public void testKeytabDuplicateWithFileName() throws OozieActionConfiguratorException, IOException, URISyntaxException, ActionExecutorException, NoSuchFieldException, IllegalAccessException {
+        final Configuration actionConf = new Configuration();
+
+        actionConf.set(SparkActionExecutor.SPARK_MASTER, "yarn");
+        actionConf.set(SparkActionExecutor.SPARK_MODE, "client");
+        actionConf.set(SparkActionExecutor.SPARK_CLASS, "org.apache.oozie.example.SparkFileCopy");
+        actionConf.set(SparkActionExecutor.SPARK_JOB_NAME, "Spark Copy File");
+        actionConf.set(SparkActionExecutor.SPARK_DEFAULT_OPTS, "defaultProperty=1\ndefaultProperty2=2\ndefaultProperty3=3");
+        actionConf.set(SparkActionExecutor.SPARK_OPTS,
+                "--principal foobar --keytab /foo/bar.keytab");
+        actionConf.set(SparkActionExecutor.SPARK_JAR, "/lib/test.jar");
+
+        final String[] mainArgs = {"arg0", "arg1"};
+        SparkArgsExtractor sparkArgsExtractor = new SparkArgsExtractor(actionConf);
+        sparkArgsExtractor.extract(mainArgs);
+        String expectedFileName = "bar.keytab";
+        assertEquals("Error happened while setting keytab presence.", sparkArgsExtractor.isKeytabPresentInSparkArgs, true);
+        assertEquals("Error happened while deciding if keytab full path given or not.", sparkArgsExtractor.isKeytabsFullPathPresentInSparkArgs, true);
+        assertEquals("File name wrongly set.", sparkArgsExtractor.keytabFileNameInSparkArgs, expectedFileName);
+    }
+
+    @Test
+    public void testKeytabDuplicateWithSymlink() throws OozieActionConfiguratorException, IOException, URISyntaxException, ActionExecutorException, NoSuchFieldException, IllegalAccessException {
+        final Configuration actionConf = new Configuration();
+
+        actionConf.set(SparkActionExecutor.SPARK_MASTER, "yarn");
+        actionConf.set(SparkActionExecutor.SPARK_MODE, "client");
+        actionConf.set(SparkActionExecutor.SPARK_CLASS, "org.apache.oozie.example.SparkFileCopy");
+        actionConf.set(SparkActionExecutor.SPARK_JOB_NAME, "Spark Copy File");
+        actionConf.set(SparkActionExecutor.SPARK_DEFAULT_OPTS, "defaultProperty=1\ndefaultProperty2=2\ndefaultProperty3=3");
+        actionConf.set(SparkActionExecutor.SPARK_OPTS,
+                "--principal foobar --keytab foo");
+        actionConf.set(SparkActionExecutor.SPARK_JAR, "/lib/test.jar");
+        final String[] mainArgs = {"arg0", "arg1"};
+        SparkArgsExtractor sparkArgsExtractor = new SparkArgsExtractor(actionConf);
+        sparkArgsExtractor.extract(mainArgs);
+        String expectedSymlink = "foo";
+
+        assertEquals("Error happened while setting keytab presence.", sparkArgsExtractor.isKeytabPresentInSparkArgs, true);
+        assertEquals("Error happened while deciding if keytab full path given or not.", sparkArgsExtractor.isKeytabsFullPathPresentInSparkArgs, false);
+        assertEquals("Symlink wrongly set.", sparkArgsExtractor.keytabSymlinkNameInSparkArgs, expectedSymlink);
+    }
+}
 
     private void checkAndDeleteFile(final String filename) {
         final File f = new File(filename);
